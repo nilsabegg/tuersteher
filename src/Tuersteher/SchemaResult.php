37c489d45bc8b9a0ccda77851796e85b7c4baca7
Nils Abegg <nils.abegg@ffuf.de>
2012-09-24 00:12:10 +0200

    Doc Blocks

---
+++ src/Tuersteher/SchemaResult.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Türsteher library.
+ */
+
+namespace Tuersteher;
+
+use Tuersteher\Exception as Exception;
+
+/**
+ * SchemaResult
+ *
+ * This class validates if a given value is a URL.
+ *
+ * @author      Nils Abegg <rueckgrat@nilsabegg.de>
+ * @version     0.1
+ * @package     Türsteher
+ * @category    Validation
+ */
+class SchemaResult
+{
+
+}
