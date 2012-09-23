e8677d93c634bfce6372ac0d49d57df6d9865961
Nils Abegg <nils.abegg@ffuf.de>
2012-09-23 19:22:32 +0200

    Doc Blocks

--- src/FFUF/Tuersteher/CustomValidator.php
+++ src/FFUF/Tuersteher/CustomValidator.php
@@ -1,7 +1,23 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of the FFUF Türsteher library.
  */
-?>
+
+namespace FFUF\Tuersteher;
+
+/**
+ * AbstractValidator
+ *
+ * This class validates if a given value is a URL.
+ *
+ * @author  Nils Abegg <rueckgrat@nilsabegg.de>
+ * @version 0.1
+ * @package Türsteher
+ * @subpackage Validator
+ * @category Validation
+ */
+abstract class CustomValidator
+{
+
+}
