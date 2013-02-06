28c76200c7ad55de68e6f3ce9c5910822c2c8456
Nils Abegg <nils.abegg@ffuf.de>
2013-02-06 21:57:40 +0100

    no message

--- tests/Tuersteher/Test/Validator/FilterValidatorTest.php
+++ tests/Tuersteher/Test/Validator/FilterValidatorTest.php
@@ -7,12 +7,12 @@ class FilterValidatorTest extends \PHPUnit_Framework_TestCase
 
     public function testAddOption()
     {
-        
+
         $validator = $this->getMockForAbstractClass('\\Tuersteher\\Validator\\FilterValidator');
         $validator->addOption('option1', 1234);
         $validator->addOption('option2', '4321');
-        $this->assertEquals($this->getOption('option1'), 1234);
-        $this->assertEquals($this->getOption('option2'), '4321');
+        $this->assertEquals($validator->getOption('option1'), 1234);
+        $this->assertEquals($validator->getOption('option2'), '4321');
 
     }
 //    public function testConcreteMethod()
