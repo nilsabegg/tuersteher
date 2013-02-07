d2a5fa674b9d935b521b3bb9672a1de28a80de29
Nils Abegg <nils.abegg@ffuf.de>
2013-02-07 03:37:14 +0100

    no message

--- tests/Tuersteher/Test/ResultTest.php
+++ tests/Tuersteher/Test/ResultTest.php
@@ -5,6 +5,28 @@ namespace Tuersteher\Test;
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testConstruct()
+    {
+
+        $isValid = true;
+        $message = 'Valid.';
+        $result = new \Tuersteher\Result($isValid, $message);
+        $this->assertEquals($isValid, $result->getIsValid());
+        $this->assertEquals($message, $result->getMessage());
+        $isValid2 = 324;
+        $this->setExpectedException('\\Tuersteher\\Exception');
+        new \Tuersteher\Result($isValid2);
+        $message2 = array();
+        $this->setExpectedException('\\Tuersteher\\Exception');
+        new \Tuersteher\Result($isValid, $message2);
+
+    }
+
+    public function testToString()
+    {
+
+    }
+    
     public function testGetAndSetIsValid()
     {
 
@@ -20,9 +42,18 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testGetOption()
+    public function testGetAndSetMessage()
     {
 
+        $message = 'Valid.';
+        $result = $result = new \Tuersteher\Result($message);
+        $this->assertEquals($message, $result->getMessage());
+        $message2 = 'Not Valid.';
+        $result->setMessage($message2);
+        $this->assertEquals($message2, $result->getMessage());
+        $message3 = array();
+        $this->setExpectedException('\\Tuersteher\\Exception');
+        $result->setMessage($message3);
 
     }
 
