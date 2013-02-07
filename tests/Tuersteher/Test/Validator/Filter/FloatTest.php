e128a2bf536ea5dd8a16434af6194ef55dfc310b
Nils Abegg <nils.abegg@ffuf.de>
2013-02-07 01:08:45 +0100

    no message

--- tests/Tuersteher/Test/Validator/Filter/FloatTest.php
+++ tests/Tuersteher/Test/Validator/Filter/FloatTest.php
@@ -8,16 +8,16 @@ class FloatTest extends \PHPUnit_Framework_TestCase
     public function testIsFloat()
     {
         $validator = new \Tuersteher\Validator\Filter\Integer();
+        $isValid = $validator->isValid(10000.12);
+        $this->assertTrue($isValid);
+        $flag = \FILTER_FLAG_ALLOW_THOUSAND;
+        $validator->setFlag($flag);
+        $isValid2 = $validator->isValid('10,000.12');
+        $this->assertTrue($isValid2);
         $options = array(
             'decimal' => ','
         );
         $validator->setOptions($options);
-        $flag = \FILTER_FLAG_ALLOW_THOUSAND;
-        $validator->setFlag($flag);
-        $isValid = $validator->isValid('10,000.12');
-        $this->assertFalse($isValid);
-        $isValid2 = $validator->isValid('10.000,12');
-        $this->assertTrue($isValid2);
 
     }
 
