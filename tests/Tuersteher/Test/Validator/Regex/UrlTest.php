a3b61500373050c172efbdc4046ec2ccec38bc98
Nils Abegg <nils.abegg@ffuf.de>
2013-02-09 06:27:33 +0100

    no message

--- tests/Tuersteher/Test/Validator/Regex/UrlTest.php
+++ tests/Tuersteher/Test/Validator/Regex/UrlTest.php
@@ -13,9 +13,8 @@ class UrlTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($isValid());
         $isValid2 = $urlValidator->validate('http://dsfdfsdff.de');
         $this->assertTrue($isValid2());
-        $urlValidator->setFlag(FILTER_FLAG_QUERY_REQUIRED);
         $isValid3 = $urlValidator->validate('http://example.com');
-        $this->assertFalse($isValid3());
+        $this->assertTrue($isValid3());
 
     }
 
