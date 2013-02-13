c9222fb3dffac314eb5a3a2512f6f6ae35785ed6
Nils Abegg <nils.abegg@ffuf.de>
2013-02-13 02:50:58 +0100

    no message

---
+++ src/Tuersteher/ExceptionInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Türsteher library.
+ */
+
+namespace Tuersteher;
+
+/**
+ * ValidatorInterface
+ *
+ * This is the interface for all validators.
+ *
+ * When you create validators which doesn't extend any
+ * of the Türsteher validators you need to implement
+ * this interface to make your validators compatible
+ * with Türsteher.
+ *
+ * @author      Nils Abegg <rueckgrat@nilsabegg.de>
+ * @version     0.1
+ * @package     Validator
+ * @category    Validation
+ */
+interface ExceptionInterface
+{
+
+}
