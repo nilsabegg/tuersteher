adb08bc7f8c88db2c20cbb034bd68ed6353619dd
Nils Abegg <nils.abegg@ffuf.de>
2013-02-15 04:07:59 +0100

    no message

--- tests/Tuersteher/Test/Validator/Filter/UrlTest.php
+++ tests/Tuersteher/Test/Validator/Filter/UrlTest.php
@@ -8,7 +8,15 @@ class UrlTest extends \PHPUnit_Framework_TestCase
     public function testIsUrl()
     {
 
-
+        $result = new \Tuersteher\Result\Validator();
+        $validator = new \Tuersteher\Validator\Filter\Url();
+        $validator->setResult($result);
+        $url1 = 'http://google.com';
+        $isValid1 = $validator->validate($url1);
+        $url2 = 'google.com';
+        $isValid2 = $validator->validate($url2);
+        $this->assertTrue($isValid1);
+        $this->assertFalse($isValid2);
 
     }
 
