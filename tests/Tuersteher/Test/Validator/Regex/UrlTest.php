e5a403063d922a15307df8bc242fc76b777de9f0
Nils Abegg <nils.abegg@ffuf.de>
2013-02-15 15:45:09 +0100

    no message

--- tests/Tuersteher/Test/Validator/Regex/UrlTest.php
+++ tests/Tuersteher/Test/Validator/Regex/UrlTest.php
@@ -17,12 +17,6 @@ class UrlTest extends \PHPUnit_Framework_TestCase
         $isValid2 = $validator->validate($url2);
         $this->assertTrue($isValid1->isValid());
         $this->assertFalse($isValid2());
-        $validator->setQueryRequired(true);
-        $isValid3 = $validator->validate($url1);
-        $this->assertFalse($isValid3);
-        $url3 = 'http://google.com?q=search';
-        $isValid4 = $validator->validate($url3);
-        $this->assertTrue($isValid4);
 
     }
 
