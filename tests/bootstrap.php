3eb9ba05c3ee8030cb736b6295fd7db2b2a3207b
Nils Abegg <nils.abegg@ffuf.de>
2013-02-16 06:14:45 +0100

    no message

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,7 @@ require_once __DIR__.'/Autoloader.php';
 $appDir = __DIR__ . '/../src';
 $appLoader = new Autoloader('Tuersteher', $appDir);
 $appLoader->register();
+
+$testDir = __DIR__ . '/..';
+$testLoader = new Autoloader('Tuersteher\\Test', $testDir);
+$testLoader->register();
