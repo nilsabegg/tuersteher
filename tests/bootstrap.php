dd5957ce05ce55aab2c6d7be818053450b30b520
Nils Abegg <nils.abegg@ffuf.de>
2013-02-16 06:16:17 +0100

    no message

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once __DIR__.'/Autoloader.php';
+$testDir = __DIR__ . '';
+$testLoader = new Autoloader('Tuersteher\\Test', $testDir);
+$testLoader->register();
 
 $appDir = __DIR__ . '/../src';
 $appLoader = new Autoloader('Tuersteher', $appDir);
 $appLoader->register();
-
-$testDir = __DIR__ . '/..';
-$testLoader = new Autoloader('Tuersteher\\Test', $testDir);
-$testLoader->register();
