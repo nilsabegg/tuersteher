a3b988d62c2626ad1dc9a17ec644ef7922bf1b51
Nils Abegg <nils.abegg@ffuf.de>
2013-02-16 06:21:55 +0100

    no message

--- tests/Tuersteher/Test/Validator.php
+++ tests/Tuersteher/Test/Validator.php
@@ -45,4 +45,18 @@ class Validator extends RegexValidator
      * @var     string
      */
     protected $regex = '/^(https?):\/\/(([a-z0-9$_\.\+!\*\'\(\),;\?&=-]|%[0-9a-f]{2})+(:([a-z0-9$_\.\+!\*\'\(\),;\?&=-]|%[0-9a-f]{2})+)?@)?(?#)((([a-z0-9]\.|[a-z0-9][a-z0-9-]*[a-z0-9]\.)*[a-z][a-z0-9-]*[a-z0-9]|((\d|[1-9]\d|1\d{2}|2[0-4][0-9]|25[0-5])\.){3}(\d|[1-9]\d|1\d{2}|2[0-4][0-9]|25[0-5]))(:\d+)?)(((\/+([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)*(\?([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)?)?)?(#([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)?$/i';
+
+    public function validate($value)
+    {
+
+        $isValid = preg_match($this->regex, $value);
+        if ($isValid != false) {
+            $this->result = $this->createResult(true, $this->messages);
+        } else {
+            $this->result = $this->createResult(false, $this->messages);
+        }
+
+        return $this->result;
+
+    }
 }
