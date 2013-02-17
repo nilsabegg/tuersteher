54e104464cdaa53b16ac9ac4f62c5607b268a532
Nils Abegg <nils.abegg@ffuf.de>
2013-02-17 22:50:23 +0100

    no message

--- src/Tuersteher/Validator/Schema.php
+++ src/Tuersteher/Validator/Schema.php
@@ -6,8 +6,8 @@
 
 namespace Tuersteher\Validator;
 
-use \Tuersteher\Exception as Exception;
-use \Tuersteher\Validator\Schema as Validator;
+use \Tuersteher\Exception\InvalidArgument as InvalidArgumentException;
+use \Tuersteher\Validator\Validator as Validator;
 /**
  * ValidationSchema
  *
@@ -36,28 +36,6 @@ class Schema extends Validator
     );
 
     /**
-     * addMessage
-     *
-     *
-     *
-     * @access  public
-     * @param   string $name
-     * @param   string $message
-     * @return  void
-     * @throws  \Tuersteher\Exception\InvalidArgument
-     */
-    public function addMessage($name, $message)
-    {
-
-        if (key_exists($name, $this->messages) == false) {
-            $this->messages[$name] = $message;
-        } else {
-            throw new InvalidArgumentException('Message "' . $name . '" allready exists');
-        }
-
-    }
-
-    /**
      * addValidator
      *
      *
@@ -82,94 +60,8 @@ class Schema extends Validator
         }
 
     }
-
-    /**
-     * getMessage
-     *
-     *
-     *
-     * @access  public
-     * @param   string $name
-     * @return  string
-     * @throws  \Tuersteher\Exception\InvalidArgument
-     */
-    public function getMessage($name = 'default')
-    {
-
-        if (key_exists($name, $this->messages) == true) {
-
-            return $this->messages[$name];
-        } else {
-            throw new InvalidArgumentException('Message "' . $name . '" doesn\'t exist');
-        }
-
-    }
-
-    /**
-     * getMessages
-     *
-     *
-     *
-     * @access  public
-     * @return  array
-     * @throws  \Tuersteher\Exception\Validator
-     */
-    public function getMessages()
-    {
-
-        if (count($this->messages) > 0) {
-
-            return $this->messages;
-        } else {
-            throw new ValidatorException('No messages set.');
-        }
-
-    }
-
-    /**
-     * setMessage
-     *
-     *
-     *
-     * @access  public
-     * @param   string $message
-     * @param   string $messageKey
-     * @return  void
-     * @throws  \Tuersteher\Exception\InvalidArgument
-     */
-    public function setMessage($message, $messageKey = 'default')
-    {
-
-        if (key_exists($messageKey, $this->messages) == true) {
-            $this->messages[$messageKey] = $message;
-        } else {
-            throw new InvalidArgumentException();
-        }
-
-    }
-
-    /**
-     * setMessages
-     *
-     *
-     *
-     * @access  public
-     * @param   array   $messages
-     * @return  void
-     * @throws  \Tuersteher\Exception\InvalidArgument
-     */
-    public function setMessages($messages)
-    {
-
-        if (is_array($messages) == true) {
-            $this->messages = $messages;
-        } else {
-            throw new InvalidArgumentException('The messages are expected as array');
-        }
-
-    }
-
-    /**
+    
+     /**
      * setValidator
      *
      *
@@ -191,6 +83,26 @@ class Schema extends Validator
             }
         } else {
             throw new Exception('No name for Validator given.');
+        }
+
+    }
+
+    /**
+     * validate
+     *
+     * Checks if the input is valid.
+     *
+     * @access  public
+     * @param   mixed   $value
+     * @return  \Tuersteher\Result\Schema
+     */
+    public function validate($value)
+    {
+
+        if (is_array($value) == true && count($value) > 0) {
+
+        } else {
+            throw new InvalidArgumentException('The passed value is not an array or the array is empty.');
         }
 
     }
