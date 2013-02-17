ba27259a94e50873e6aed404c829dbe61e7f8210
Nils Abegg <nils.abegg@ffuf.de>
2013-02-17 06:53:45 +0100

    no message

--- tests/Tuersteher/Test/Result/SchemaTest.php
+++ tests/Tuersteher/Test/Result/SchemaTest.php
@@ -22,51 +22,7 @@ class SchemaTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($results, $result->getResults());
         $results2 = 324;
         $this->setExpectedException('\\Tuersteher\\Exception\\InvalidArgument');
-        new \Tuersteher\Result\Validator($isValid, $message, $results2);
-
-    }
-
-    public function testToString()
-    {
-
-        $result = new \Tuersteher\Result\Schema(true);
-        $this->assertEquals("$result", 'Is valid.');
-        $result->setIsValid(false);
-        $this->assertEquals("$result", 'Is not valid.');
-        $result->setMessage('Blabla');
-        $this->assertEquals("$result", 'Blabla');
-        $this->setExpectedException('\\Tuersteher\\Exception\\InvalidArgument');
-        new \Tuersteher\Result\Schema(true, array());
-
-    }
-
-    public function testGetAndSetIsValid()
-    {
-
-        $isValid = true;
-        $result = new \Tuersteher\Result\Schema($isValid);
-        $this->assertEquals($isValid, $result->isValid());
-        $isValid2 = false;
-        $result->setIsValid($isValid2);
-        $this->assertEquals($isValid2, $result->isValid());
-        $isValid3 = 'dsfsfsddf';
-        $this->setExpectedException('\\Tuersteher\\Exception\\InvalidArgument');
-        $result->setIsValid($isValid3);
-
-    }
-//
-    public function testGetAndSetMessage()
-    {
-
-        $message = 'Valid.';
-        $result = new \Tuersteher\Result\Schema(true, $message);
-        $this->assertEquals($message, $result->getMessage());
-        $message2 = 'Not Valid.';
-        $result->setMessage($message2);
-        $this->assertEquals($message2, $result->getMessage());
-        $message3 = array();
-        $this->setExpectedException('\\Tuersteher\\Exception\\InvalidArgument');
-        $result->setMessage($message3);
+        new \Tuersteher\Result\Schema($isValid, $message, $results2);
 
     }
 
@@ -85,13 +41,4 @@ class SchemaTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testNoMessageException()
-    {
-
-        $result = new \Tuersteher\Result\Schema();
-        $this->setExpectedException('\\Tuersteher\\Exception\\Result');
-        $noFail = "$result";
-        $result->getMessage();
-
-    }
 }
