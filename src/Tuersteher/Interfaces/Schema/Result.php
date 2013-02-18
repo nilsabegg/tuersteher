690e392230d077cfb1e0210ceffdceaa69f1ab74
Nils Abegg <nils.abegg@ffuf.de>
2013-02-18 05:27:07 +0100

    no message

--- src/Tuersteher/Interfaces/Schema/Result.php
+++ src/Tuersteher/Interfaces/Schema/Result.php
@@ -6,6 +6,7 @@
 
 namespace Tuersteher\Interfaces\Schema;
 
+use \Tuersteher\Interfaces\Result as ValidatorResult;
 /**
  * ValidatorInterface
  *
@@ -22,76 +23,7 @@ namespace Tuersteher\Interfaces\Schema;
  * @subpackage  Result
  * @category    Validation
  */
-interface Result
+interface Result extends ValidatorResult
 {
-
-    /**
-     * __toString
-     *
-     * Returns the string which is used when the
-     * result is used as a string.
-     * It's a shortcut for getMessage().
-     * @access  public
-     * @return  string
-     */
-    public function __toString();
-
-    /**
-     * __invoke
-     *
-     * Returns if the the result is valid or not valid
-     * when the result object is used as method.
-     * It's a shortcut for isValid().
-     * <code>
-     * if ($result()) {
-     *  //do blabla
-     * }
-     * </code>
-     *
-     * @access  public
-     * @return  boolean
-     */
-    public function __invoke();
-
-    /**
-     * getMessage
-     *
-     * Returns the message of the result.
-     *
-     * @access  public
-     * @return  string
-     */
-    public function getMessage();
-
-    /**
-     * isValid
-     *
-     * Returns the actual result if it is set.
-     *
-     * @access  public
-     * @return  boolean
-     */
-    public function isValid();
-
-    /**
-     * setIsValid
-     *
-     * Set the actual result of the result object.
-     *
-     * @access  public
-     * @param   boolean $valid
-     * @return  void
-     */
-    public function setValid($valid);
-
-    /**
-     * setMessage
-     *
-     * Set the message of the result.
-     *
-     * @access  public
-     * @param   string  $message
-     * @return  void
-     */
-    public function setMessage($message);
+  
 }
