bbb26169e4aab6a27fdc5235e421df678fb01a40
Nils Abegg <nils.abegg@ffuf.de>
2013-03-17 19:00:18 +0100

    no message

--- src/Tuersteher/Validator/Filter/Integer.php
+++ src/Tuersteher/Validator/Filter/Integer.php
@@ -48,4 +48,89 @@ class Integer extends FilterValidator
     protected $messages = array(
         'default' => 'The input was not an integer.'
     );
+
+    /**
+     * getMax
+     *
+     *
+     *
+     * @access public
+     * @return string
+     */
+    public function getMax()
+    {
+
+        if (isset($this->options['max']) == true) {
+            return $this->options['max'];
+        } else {
+            throw new FilterException('The maximum value is not set.');
+        }
+
+    }
+
+    /**
+     * getMax
+     *
+     *
+     *
+     * @access public
+     * @return string
+     */
+    public function getMin()
+    {
+
+        if (isset($this->options['min']) == true) {
+            return $this->options['min'];
+        } else {
+            throw new FilterException('The minimum value is not set.');
+        }
+
+    }
+
+    /**
+     * setMax
+     *
+     *
+     *
+     * @access public
+     * @param  string   $max
+     * @return void
+     * @throws \Tuersteher\Exception\InvalidArgument
+     */
+    public function setMax($max)
+    {
+
+        if ($max == '') {
+            unset($this->options['max']);
+        } elseif (is_int($max) == true) {
+            $this->options['max'] = $seperator;
+        } else {
+            throw new InvalidArgumentException('The passed maximum value is not an integer.');
+        }
+
+    }
+
+    /**
+     * setMin
+     *
+     *
+     *
+     * @access public
+     * @param  string   $min
+     * @return void
+     * @throws \Tuersteher\Exception\InvalidArgument
+     */
+    public function setMin($min)
+    {
+
+        if ($min == '') {
+            unset($this->options['min']);
+        } elseif (is_int($min) == true) {
+            $this->options['min'] = $seperator;
+        } else {
+            throw new InvalidArgumentException('The passed minimum value is not an integer.');
+        }
+
+    }
+
 }
