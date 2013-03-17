fcfe45cc55b8f620298af01586035901c16873e9
Nils Abegg <nils.abegg@ffuf.de>
2013-03-17 19:58:43 +0100

    no message

--- tests/Tuersteher/Test/Validator/Filter/IntegerTest.php
+++ tests/Tuersteher/Test/Validator/Filter/IntegerTest.php
@@ -8,6 +8,28 @@ class IntegerTest extends \PHPUnit_Framework_TestCase
     public function testIsInteger()
     {
 
+        $result = new \Tuersteher\Result\Validator();
+        $validator = new \Tuersteher\Validator\Filter\Integer();
+        $validator->setResult($result);
+        $integer1 = 2.30;
+        $isValid1 = $validator->validate($integer1);
+        $this->assertFalse($isValid1());
+        $integer2 = 230;
+        $isValid2 = $validator->validate($integer2);
+        $this->assertTrue($isValid2());
+        $integer3 = 'string';
+        $isValid3 = $validator->validate($integer3);
+        $this->assertFalse($isValid3());
+        $validator->setMin(6);
+        $integer4 = 4;
+        $isValid4 = $validator->validate($integer4);
+        $this->assertFalse($isValid4());
+        $validator->setMax(220);
+        $isValid5 = $validator->validate($integer2);
+        $this->assertFalse($isValid5());
+        $integer5 = 200;
+        $isValid6 = $validator->validate($integer5);
+        $this->assertFalse($isValid6());
 
     }
 
