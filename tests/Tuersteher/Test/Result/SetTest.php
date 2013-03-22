3f7a5793d4e5bc76886d2b437934eeb41bbe4631
Nils Abegg <nils.abegg@ffuf.de>
2013-03-22 21:38:14 +0100

    no message

--- tests/Tuersteher/Test/Result/SetTest.php
+++ tests/Tuersteher/Test/Result/SetTest.php
@@ -2,8 +2,8 @@
 
 namespace Tuersteher\Test\Result;
 
-class SchemaTest extends \Tuersteher\Test\Result\SchemaTest
+class SetTest extends \Tuersteher\Test\Result\SchemaTest
 {
-    
+
     protected $resultClass = '\\Tuersteher\\Result\\Set';
 }
